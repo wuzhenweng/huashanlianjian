02cc00f62432f40c3d5f636edb08ae2ef13aed61
wzw <15186785289@qq.com>
2021-04-01 19:06:13 +0800

    第一个安装测试程序

---
+++ src/main/java/com/atiguigu/maven/Apple.java
@@ -0,0 +1,10 @@
+package com.atiguigu.maven;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("第一个测试");
+	}
+
+}
